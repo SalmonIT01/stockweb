500c07534f51a9942855e79a03d2ff3f360ef442
Panupong <101684618+SalmonIT01@users.noreply.github.com>
2023-11-08 15:33:08 +0700

    fix

--- StockWebApp/app_home/admin.py
+++ StockWebApp/app_home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Details
+from .models import Details,Unit
 
 # Register your models here.
 admin.site.register(Details)
+admin.site.register(Unit)
--- StockWebApp/app_home/models.py
+++ StockWebApp/app_home/models.py
@@ -7,5 +7,10 @@ class Details(models.Model):
     product_name = models.CharField(max_length = 20, default = '', null = False)
     unit_id = models.CharField(max_length = 20, default = '', null = False)
     amount = models.CharField(max_length = 20, default = '', null = False)
-    status_id = models.CharField(max_length = 20, default = '', null = False)
+    status_id = models.IntegerField(max_length = 20, default = '', null = False)
+
+class Unit(models.Model):
+    unit_id = models.IntegerField(max_length = 20, default = '', null = False)
+    unit_name = models.CharField(max_length = 225, default = '', null = False)    
+
 
