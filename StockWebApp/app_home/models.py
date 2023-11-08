05d218dbd774af28628a00f1e67748ea1274769b
Nonz466 <thittipong07366@gmail.com>
2023-11-08 14:13:47 +0700

    เพิ่มตาราง

--- StockWebApp/app_home/models.py
+++ StockWebApp/app_home/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Details(models.Model):
     product_id = models.CharField(max_length = 20, default = '', null = False)
-    product_name = models.CharField(max_length = 20, default = '', null = False) 
+    product_name = models.CharField(max_length = 20, default = '', null = False)
+    unit_id = models.CharField(max_length = 20, default = '', null = False)
+    amount = models.CharField(max_length = 20, default = '', null = False)
+    status_id = models.CharField(max_length = 20, default = '', null = False)
