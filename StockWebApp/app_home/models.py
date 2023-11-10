b29d08509f005a050f038b92cfa6227828ec1505
SalmonIT01 <101684618+SalmonIT01@users.noreply.github.com>
2023-11-10 10:39:27 +0700

    add bomb

--- StockWebApp/app_home/models.py
+++ StockWebApp/app_home/models.py
@@ -12,3 +12,5 @@ class Details(models.Model):
 class Unit(models.Model):
     unit_id = models.IntegerField(max_length = 20, default = '', null = False)
     unit_name = models.CharField(max_length = 225, default = '', null = False)    
+
+
